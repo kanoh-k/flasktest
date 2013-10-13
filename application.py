381d1727af5c38fcbef70f53fa95ca46ba215637
Kyohei <k.futian@gmail.com>
2013-10-13 12:22:40 +0000

    subpage test

--- application.py
+++ application.py
@@ -1,14 +1,18 @@
 import flask
  
-application = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
 #Set application.debug=true to enable tracebacks on Beanstalk log output. 
 #Make sure to remove this line before deploying to production.
 application.debug=True
  
-@application.route('/')
+@app.route('/')
 def hello_world():
     return "Hello world!"
+
+@app.route('/sub/<user>')
+def sub_page(user):
+    return "This is a sub page for %s!" % user
  
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
