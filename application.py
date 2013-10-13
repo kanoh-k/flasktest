16529aca2f1ea315e66c98bfab5c9ae3cfaa477f
Kyohei <k.futian@gmail.com>
2013-10-13 12:40:11 +0000

    user page

--- application.py
+++ application.py
@@ -13,6 +13,10 @@ def hello_world():
 @application.route('/sub')
 def sub_page():
     return "Sub page!"
+
+@application.route('/user/<user>')
+def user_page(user):
+    return user
  
 if __name__ == '__main__':
     application.run(host='0.0.0.0', debug=True)
